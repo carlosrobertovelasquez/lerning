056b0973e7769ba3c2aa88e24b70e24ee62a5877
cvelasquez <carlosrobertovelasquez@gmail.com>
2019-11-21 22:24:09 -0600

    Capitulo 9-026

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,9 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::get('/set_languge/{Lang}','Controller@setLanguage')->name('set_language');
+
 Route::get('login/{driver}', 'Auth\LoginController@redirectToProvider')->name('social_auth');
 Route::get('login/{driver}/callback', 'Auth\LoginController@handleProviderCallback');
 
